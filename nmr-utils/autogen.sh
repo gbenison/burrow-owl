3ba1da78e35bfc348dd7ae2a82ef504730d81fa4
Greg Benison <gbenison@gmail.com>
2007-06-18 17:26:41 -0700

    added autogen for nmr-utils

--- nmr-utils/autogen.sh
+++ nmr-utils/autogen.sh
@@ -3,14 +3,6 @@
 # Run this file so that a fresh checkout from
 # source control can be configured & built.
 
-for a in *;
-do
-   test -d $a || continue;
-   test -f $a/autogen.sh || continue;
-   (cd $a; ./autogen.sh;)
-done
-
-libtoolize;
 aclocal;
 automake --add-missing;
 autoconf;
