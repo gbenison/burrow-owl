669d5228edc77ff7d69c468235968a99e2bcdfcb
Greg Benison <gbenison@gmail.com>
2007-12-26 16:41:01 -0800

    changed fsm compiler to the Moore model

--- src/finite-state-machine/contour-fsm.h
+++ src/finite-state-machine/contour-fsm.h
@@ -8,18 +8,27 @@ struct hos_point {gdouble x, y;};
 
 void test_method(void*, struct hos_point*, const int, const int, gboolean);
 
+typedef void (*trace_func_t)(void* data, struct hos_point* points,
+			   const gint n_point,
+			   gint level,
+			   gboolean closed);
 
-void contour_fsm(double *buf_base,
-		 const int x_offset,
-		 const int y_offset,
-		 const int stride,
-		 const int nx,
-		 const int ny,
-		 double *levels,
-		 const int n_levels,
-		 void (*trace_method)(void* data, struct hos_point*, const gint n_point, gint level, gboolean closed),
-		 void *trace_data,
-		 gulong *cancellation);
+typedef struct _fsm_state fsm_state_t;
+
+gboolean contour_fsm(fsm_state_t* state);
+void fsm_state_free(fsm_state_t* state);
+fsm_state_t*
+fsm_state_init(double* buffer,
+	       int nx,
+	       int ny,
+	       int x_first,
+	       int x_last,
+	       int y_first,
+	       int y_last,
+	       double* levels,
+	       int n_levels,
+	       trace_func_t trace_method,
+	       gpointer trace_data);
 
 #endif /* HAVE_CONTOUR_FSM */
 
