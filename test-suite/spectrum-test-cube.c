f80b4daa944a3533574cbaececd835910f9ccaec
Greg Benison <gbenison@gmail.com>
2008-04-01 07:26:23 -0700

    added 3d test spectrum

---
+++ test-suite/spectrum-test-cube.c
@@ -0,0 +1,48 @@
+
+#include "spectrum-test-cube.h"
+
+static gdouble  test_cube_accumulate (HosSpectrum* self, HosSpectrum* root, guint* idx);
+static gboolean test_cube_tickle     (HosSpectrum* self, HosSpectrum* root, guint* idx, gdouble* dest);
+
+G_DEFINE_TYPE (HosSpectrumTestCube, hos_spectrum_test_cube, HOS_TYPE_SPECTRUM)
+
+static const int default_np = 32;
+
+static void
+hos_spectrum_test_cube_class_init(HosSpectrumTestCubeClass *klass)
+{
+  HosSpectrumClass* spectrum_class = HOS_SPECTRUM_CLASS(klass);
+
+  spectrum_class->tickle     = test_cube_tickle;
+  spectrum_class->accumulate = test_cube_accumulate;
+}
+
+static void
+hos_spectrum_test_cube_init(HosSpectrumTestCube *self)
+{
+  HosSpectrum* spectrum = HOS_SPECTRUM(self);
+  spectrum->ndim  = 3;
+  spectrum->np    = g_new0(gint, 3);
+  spectrum->np[0] = default_np;
+  spectrum->np[1] = default_np;
+  spectrum->np[2] = default_np;
+}
+
+static gdouble
+test_cube_accumulate (HosSpectrum* self, HosSpectrum* root, guint* idx)
+{
+  return idx[0] + 100 * idx[1] + 10000 * idx[2];
+}
+
+static gboolean
+test_cube_tickle (HosSpectrum* self, HosSpectrum* root, guint* idx, gdouble* dest)
+{
+  *dest = test_cube_accumulate(self, root, idx);
+  return TRUE;
+}
+
+HosSpectrumTestCube*
+spectrum_test_cube_new()
+{
+  return HOS_SPECTRUM_TEST_CUBE(g_object_new(HOS_TYPE_SPECTRUM_TEST_CUBE, NULL));
+}
--- test-suite/t_spectrum.c
+++ test-suite/t_spectrum.c
@@ -1,6 +1,7 @@
 
 #include <burrow.h>
 #include "spectrum-ramp.h"
+#include "spectrum-test-cube.h"
 
 int
 main()
@@ -9,14 +10,29 @@ main()
   if (!g_thread_supported ()) g_thread_init (NULL);
 
   HosSpectrumRamp *spec = spectrum_ramp_new();
-  g_message("Made new ramped spectrum with %d dimensions\n", HOS_SPECTRUM(spec)->ndim);
+  g_message("Made new ramped spectrum with %d dimensions\n", spectrum_ndim(spec));
+
+  HosSpectrumTestCube *cube = spectrum_test_cube_new();
+  g_message("Made new test cube spectrum with %d dimensions\n", spectrum_ndim(cube));
 
   spectrum_traverse_blocking(HOS_SPECTRUM(spec));
-  g_print("Values: ");
+  g_print("Ramp values: ");
   int i;
   for (i = 0; i < 10; ++i)
     g_print("%f ", spectrum_peek(HOS_SPECTRUM(spec), i));
   g_print("...\n");
+
+  spectrum_traverse_blocking(HOS_SPECTRUM(cube));
+  gint start;
+  static const gint n = 10;
+  for (start = 1; start < 10000; start *= 4)
+    {
+      gint i;
+      g_print("Cube (%6d, ...) = ", start);
+      for (i = 0; i < n; ++i)
+	g_print("%f ", spectrum_peek(cube, start + i));
+      g_print("\n");
+    }
 
   return 0;
 }
