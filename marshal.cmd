64a4171788a3dc6a8c0f3f0d7f49e46a943be3b0
Greg Benison <gbenison@gmail.com>
2008-07-15 15:37:05 -0700

    split graphical components into a separate module

---
+++ marshal.cmd
@@ -0,0 +1,4 @@
+# put here specifications for custom
+# closure signatures, for example:
+## VOID:DOUBLE,DOUBLE
+
--- src/gw/spectrum-spec.scm
+++ src/gw/spectrum-spec.scm
@@ -11,41 +11,28 @@
 
 (define-class <spectrum-wrapset> (<gobject-wrapset-base>)
   #:id 'spectrum
-  #:dependencies '(standard gnome-glib gnome-gobject gnome-gdk gnome-gtk))
+  #:dependencies '(standard gnome-glib gnome-gobject))
 
 (define-method (global-declarations-cg (self <spectrum-wrapset>))
   (list (next-method)
         "#include <burrow/spectrum.h>\n"
-        "#include <hoscanvas.h>\n"
-        "#include <canvasitem.h>\n"
-        "#include <marker.h>\n"
-        "#include <marker_text.h>\n"
-        "#include <cursor.h>\n"
-        "#include <contourplot.h>\n"
         "#include <version.h>\n"
         "#include <contour.h>\n"
-	"#include <grid.h>\n"
         "#include <painter.h>\n"
         "#include <painter_bwps.h>\n"
-        "#include <painter_gdk.h>\n"
         "#include <burrow/nih.h>\n"
         "#include <hosbackingsim.h>\n"
         "\n"))
 
-        ; couple of disabled sources
-        ; "#include <box.h>\n"
-        ; "#include <speciter.h>\n"
-
 #!
 (define-method (global-definitions-cg (self <spectrum-wrapset>))
   (list (next-method)))
-
 !#
+
 (define-method (initializations-cg (self <spectrum-wrapset>) err)
   (list
    "{if (!g_thread_supported ()) g_thread_init (NULL);}"
    (next-method)))
-
 
 (define-method (initialize (ws <spectrum-wrapset>) initargs)
   (next-method ws (cons #:module (cons '(spectrum) initargs)))
