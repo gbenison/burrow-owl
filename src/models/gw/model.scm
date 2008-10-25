af42801b96e97966ff271fedc9d3dc9abfdf1ab0
greg <greg@gblaptop>
2008-10-25 11:32:20 -0700

    un-merged models to continue development on a separate branch

--- src/models/gw/model.scm
+++
@@ -1,9 +0,0 @@
-
-(define-module (burrow model))
-(use-modules (oop goops)
-	     (gnome gobject)
-	     (burrow model-gw))
-
-;; re-export bindings from g-wrap
-(module-use! (module-public-interface (current-module))
-	     (resolve-interface '(burrow model-gw)))
--- src/models/gw/run-g-wrap.scm
+++
@@ -1,6 +0,0 @@
-
-(use-modules (g-wrap)
-	     (g-wrap guile)
-	     (model-spec))
-
-(generate-wrapset 'guile 'burrowmodel "model-gw")
