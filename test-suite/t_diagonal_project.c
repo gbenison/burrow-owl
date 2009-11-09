7e541092043fed62c66ba4112895d6fa307b6d56
greg <greg@gblaptop>
2009-11-08 19:17:16 -0800

    t_diagonal_project: test was failing due to fencepost error

--- test-suite/t_diagonal_project.c
+++ test-suite/t_diagonal_project.c
@@ -4,6 +4,7 @@
 #include "spectrum-test-cube.h"
 #include "spectrum-flaky.h"
 
+
 int
 main()
 {
@@ -16,36 +17,24 @@ main()
   HosSpectrum *S2 = spectrum_flakify(S1, 1.0 - 2e-4);
   HosSpectrum *S3 = spectrum_diagonal_project(S2);
 
-  gint nx = spectrum_np(S3, 0);
-
-  gint stride_x1 = spectrum_np(S2, 0);
-  gint stride_y  = spectrum_np(S2, 0) * spectrum_np(S2, 1);
+  gint s2_idx = 0;
+  gint s2_np = spectrum_np_total(S2);
 
   gint i;
   for (i = 0; i < spectrum_np_total(S3); ++i)
     {
       gdouble actual = spectrum_peek(S3, i);
+      
+      while (1)
+	{
+	  if (s2_idx >= s2_np)
+	    g_error("idx %d: cannot find matching point in source spec", i);
+	  gdouble s2_actual = spectrum_peek(S2, s2_idx);
+	  if (fabs(actual - s2_actual) < 0.01)
+	    break;
+	  ++s2_idx;
+	}
 
-      gint x = i % nx;
-      gint y = i / nx;
-
-      gdouble x_ppm = spectrum_pt2ppm(S3, 0, x);
-
-      gint source_x0 = spectrum_ppm2pt(S2, 0, x_ppm);
-      gint source_x1 = spectrum_ppm2pt(S2, 1, x_ppm);
-
-      gint source_idx_1 = source_x0 + (source_x1 - 1) * stride_x1 + y * stride_y;
-      gint source_idx_2 = source_x0 + source_x1 * stride_x1 + y * stride_y;
-      gint source_idx_3 = source_x0 + (source_x1 + 1) * stride_x1 + y * stride_y;
-
-      gdouble error_1 = fabs(actual - spectrum_peek(S2, source_idx_1));
-      gdouble error_2 = fabs(actual - spectrum_peek(S2, source_idx_2));
-      gdouble error_3 = fabs(actual - spectrum_peek(S2, source_idx_3));
-
-      if ((error_1 > 0.00001) &&
-	  (error_2 > 0.00001) &&
-	  (error_3 > 0.00001))
-	g_error("Error: idx %d actual %f, errors %f, %f, %f", i, actual, error_1, error_2, error_3);
       if ((i % 100) == 0) g_print(".");
     }
   
